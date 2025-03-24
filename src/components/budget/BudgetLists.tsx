7b180f497975392bd14554d8ee0419015f659c95
Princess-Morris <okpataprincessmorris3@gmail.com>
2025-03-24 05:31:12 +0100

    added a delete function

--- src/components/budget/BudgetLists.tsx
+++ src/components/budget/BudgetLists.tsx
@@ -4,6 +4,7 @@ import CreateList from "../form/CreateList";
 import './budget-list.css'
 
 interface listProps {
+    id: string;
     category: "Groceries" | "Laundry" | "Beddings" | "Outfits" | "Tfare" | "Giftings" | "Vacation" | "Outings" | "Repairs";
     description: string;
     amount: number;
@@ -12,11 +13,36 @@ interface listProps {
 
 
 export default function BudgetList() {
-    const list: listProps[] = []
+    const list: listProps[] = [
+        {
+            id: "1",
+            category: "Beddings",
+            description: "got 3 heavy blankets",
+            amount: 3000,
+            dateTime: "6/11/2025",
+        },
+
+        {
+            id: "2",
+            category: "Groceries",
+            description: "got a pack of milk shake",
+            amount: 15000,
+            dateTime: "7-10-2025",
+        },
+
+        {
+            id: "3",
+            category: "Repairs",
+            description: "Flooring design",
+            amount: 13000,
+            dateTime: "2025/11/05",
+        }
+    ]
 
     const [allList, setAllList] = useState<listProps[]>(list)
 
     const [formData, setFormData] = useState<listProps>({
+        id: "",
         category: "Groceries",
         description: "",
         amount: 0,
@@ -37,11 +63,19 @@ export default function BudgetList() {
         setAllList([...allList, formData])
         console.log("added")
         setFormData({
+            id: "",
             category: 'Groceries',
             description: "",
             amount: 0,
             dateTime: ""
         })
+    }
+
+    const handleDelete = (id: any) => {
+        console.log("i want to delete this post")
+        setAllList(allList.filter((list) => (list.id !== id)))
+        // allList.filter((list) => (list.id !== id))
+        console.log("I have deleted this post")
     }
 
     return (
@@ -60,11 +94,13 @@ export default function BudgetList() {
             <div className="list-wrapper">
                 {allList.map((list, key) => (
                     <div className="list" key={key}>
-                        <h3>Category: <span className="text">{list.category} </span> </h3>
+                        <h3>Category: <span className="text">{list.category} </span> </h3> 
                         <h3>Description: <span className="text">{list.description} </span> </h3>
                         <h3>Amount($): <span className="text">{list.amount} </span> </h3>
                         <h3>Date & Time: <span className="text">{list.dateTime} </span> </h3>
-                        <br />
+                        <div
+                        onClick={() => handleDelete(list.id)}
+                         className="delete-wrapper"><span>x</span></div>
                     </div>
                 ))}
             </div>
