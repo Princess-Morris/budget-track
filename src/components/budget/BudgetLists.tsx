27eda5a6911ad59800c6e3b0f9f7a2844823fe04
Princess-Morris <okpataprincessmorris3@gmail.com>
2025-03-24 17:11:37 +0100

    added update functionality to each item on the list

--- src/components/budget/BudgetLists.tsx
+++ src/components/budget/BudgetLists.tsx
@@ -19,7 +19,7 @@ export default function BudgetList() {
             category: "Beddings",
             description: "got 3 heavy blankets",
             amount: 3000,
-            dateTime: "6/11/2025",
+            dateTime: "2025-11-03",
         },
 
         {
@@ -27,7 +27,7 @@ export default function BudgetList() {
             category: "Groceries",
             description: "got a pack of milk shake",
             amount: 15000,
-            dateTime: "7-10-2025",
+            dateTime: "2025-04-03",
         },
 
         {
@@ -35,7 +35,7 @@ export default function BudgetList() {
             category: "Repairs",
             description: "Flooring design",
             amount: 13000,
-            dateTime: "2025/11/05",
+            dateTime: "2025-11-05",
         }
     ]
 
@@ -58,10 +58,19 @@ export default function BudgetList() {
     }
 
     const handleSubmit = (e: any) => {
-        console.log("adding")
+        // console.log("adding")
         e.preventDefault()
-        setAllList([...allList, formData])
-        console.log("added")
+        if (formData.id){
+            setAllList(prev => prev.map((item) => item.id === formData.id ? formData : item)) 
+            console.log(formData.id)
+        } else {
+            const newItem = {
+                ...formData,
+                id: Date.now().toString()
+            }
+            setAllList(prev => [...prev, newItem])
+        }
+        // setAllList([...allList, formData])
         setFormData({
             id: "",
             category: 'Groceries',
@@ -72,35 +81,47 @@ export default function BudgetList() {
     }
 
     const handleDelete = (id: any) => {
-        console.log("i want to delete this post")
+        // console.log("i want to delete this post")
         setAllList(allList.filter((list) => (list.id !== id)))
-        // allList.filter((list) => (list.id !== id))
-        console.log("I have deleted this post")
+        // console.log("I have deleted this post")
+    }
+
+    const handleEdit = (list: any) => {
+        // console.log("About to update an item")
+        setFormData(list)
     }
 
     return (
         <div className="form-and-list">
             <div className="form-wrapper">
-            <CreateList
-                category={formData.category}
-                description={formData.description}
-                amount={formData.amount}
-                dateTime={formData.dateTime}
-                handleChange={handleChange}
-                handleSubmit={handleSubmit} />
+                <CreateList
+                    category={formData.category}
+                    description={formData.description}
+                    amount={formData.amount}
+                    dateTime={formData.dateTime}
+                    handleChange={handleChange}
+                    handleSubmit={handleSubmit} />
 
             </div>
-            
+
             <div className="list-wrapper">
                 {allList.map((list, key) => (
                     <div className="list" key={key}>
-                        <h3>Category: <span className="text">{list.category} </span> </h3> 
+                        <h3>Category: <span className="text">{list.category} </span> </h3>
                         <h3>Description: <span className="text">{list.description} </span> </h3>
                         <h3>Amount($): <span className="text">{list.amount} </span> </h3>
                         <h3>Date & Time: <span className="text">{list.dateTime} </span> </h3>
                         <div
-                        onClick={() => handleDelete(list.id)}
-                         className="delete-wrapper"><span>x</span></div>
+                            onClick={() => handleDelete(list.id)}
+                            className="delete-wrapper">
+                            x
+                        </div>
+                        <div
+                        onClick={() => handleEdit(list)}
+                            className="edit-wrapper"
+                        >
+                            !
+                        </div>
                     </div>
                 ))}
             </div>
--- src/components/form/CreateList.tsx
+++ src/components/form/CreateList.tsx
@@ -1,77 +1,77 @@
 import "./create-list.css"
 
-interface formProps{
+interface formProps {
     category: string;
     description: string;
     amount: number;
     dateTime: string;
     handleChange: (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => void
-    handleSubmit: (e: any) => void
+    handleSubmit: (e: React.FormEvent) => void
 }
 
-export default function CreateList({ category, description, amount, dateTime, handleChange, handleSubmit}: formProps) {
+export default function CreateList({ category, description, amount, dateTime, handleChange, handleSubmit }: formProps) {
 
 
     return (
-        <form className="form" 
-        onSubmit={handleSubmit}
-        >           
-               <div className="input">
-               <label>Description: </label>
-           
-            <textarea
-            className="text-area"
-            placeholder="I got a ..."
-            name="description"
-            value={description}
-            onChange={handleChange}
-            > 
+        <form className="form"
+            onSubmit={handleSubmit}
+        >
+            <div className="input">
+                <label>Description: </label>
 
-            </textarea>
-               </div>
-            
-               <div className="input">
-               <label>Amount: </label>
-            <input 
-            type="text"
-            name="amount" 
-            value={amount}
-            onChange={handleChange}
-            /> 
-               </div>
-          
-              <div className="input">
-              <label>Date & Time: </label>
-            <input 
-            type="date"
-            name="dateTime" 
-            value={dateTime}
-            onChange={handleChange}
-            /> 
-              </div>
+                <textarea
+                    className="text-area"
+                    placeholder="I got a ..."
+                    name="description"
+                    value={description}
+                    onChange={handleChange}
+                >
 
-              <div className="input">
-            <label>Category: </label>
-            <select 
-            value={category} 
-            name="category"
-            onChange={handleChange}
-            >
-                <option value="Groceries">Groceries</option>
-                <option value="Laundry">Laundry</option>
-                <option value="Beddings">Beddings</option>
-                <option value="Outfits">Outfits</option>
-                <option value="Tfare">Tfare</option>
-                <option value="Giftings">Giftings</option>
-                <option value="Vacation">Vacation</option>
-                <option value="Outings">Outings</option>
-                <option value="Repairs">Repairs</option>
-
-            </select>
+                </textarea>
             </div>
-           
 
-            <button type="submit">Add To List</button>
+            <div className="input">
+                <label>Amount: </label>
+                <input
+                    type="text"
+                    name="amount"
+                    value={amount}
+                    onChange={handleChange}
+                />
+            </div>
+
+            <div className="input">
+                <label>Date & Time: </label>
+                <input
+                    type="Date"
+                    name="dateTime"
+                    value={dateTime}
+                    onChange={handleChange}
+                />
+            </div>
+
+            <div className="input">
+                <label>Category: </label>
+                <select
+                    value={category}
+                    name="category"
+                    onChange={handleChange}
+                >
+                    <option value="Groceries">Groceries</option>
+                    <option value="Laundry">Laundry</option>
+                    <option value="Beddings">Beddings</option>
+                    <option value="Outfits">Outfits</option>
+                    <option value="Tfare">Tfare</option>
+                    <option value="Giftings">Giftings</option>
+                    <option value="Vacation">Vacation</option>
+                    <option value="Outings">Outings</option>
+                    <option value="Repairs">Repairs</option>
+
+                </select>
+            </div>
+
+
+            <button type="submit">{category ? "update Item" : "Add To List"}</button>
 
         </form>
     )
